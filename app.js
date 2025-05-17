74bf174ce45419b31c762b237458643cced8539d
Przemek <przemyslaw.broda.95@gmail.com>
2025-05-17 16:04:07 +0200

    feat: added electron

---
+++ app.js
@@ -0,0 +1,21 @@
+﻿const {
+  app,
+  BrowserWindow
+} = require('electron')
+
+let appWindow
+
+function createWindow () {
+  appWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
+
+  appWindow.loadFile('dist/hanzi-practice-sheet-gen/browser/index.html')
+
+  appWindow.on('closed', () => {
+    appWindow = null
+  })
+}
+
+app.whenReady().then(() => createWindow())
